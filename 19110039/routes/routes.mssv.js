19378a33038c57d522e55eefa5c76e8323016cdc
Hồ Huy Luật <113324644+hohuyluat123@users.noreply.github.com>
2022-09-26 21:14:00 +0700

    Update routes.mssv.js

--- 19110039/routes/routes.mssv.js
+++ 19110039/routes/routes.mssv.js
@@ -3,9 +3,9 @@ const {mssvRequest} = require('../controllers/controller.mssvcontroller');
 const {postmssvRequest} = require('../controllers/controller.postmssvcontroller');
 const router = express.Router();
 
-// define the home page route
+
 router.get('/:mygroupID', mssvRequest);
 router.post('/:myID', postmssvRequest);
-// define the about route
+
 
 module.exports = router
