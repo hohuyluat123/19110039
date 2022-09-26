36d179a338a6bb3d0edcbd152464a7b68583f96b
Hồ Huy Luật <113324644+hohuyluat123@users.noreply.github.com>
2022-09-26 21:14:21 +0700

    Update routes.mygroup.js

--- 19110039/routes/routes.mygroup.js
+++ 19110039/routes/routes.mygroup.js
@@ -2,9 +2,8 @@ const express = require('express');
 const {mygroupRequest} = require('../controllers/controller.mygroupcontroller');
 const router = express.Router();
 
-// define the home page route
+
 router.get('/', mygroupRequest);
 
-// define the about route
 
 module.exports = router
