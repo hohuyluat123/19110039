338f86accbfa6922b6578d2a65b8a2ec0ba948f6
Hồ Huy Luật <113324644+hohuyluat123@users.noreply.github.com>
2022-09-26 21:13:42 +0700

    Update routes.message.js

--- 19110039/routes/routes.message.js
+++ 19110039/routes/routes.message.js
@@ -3,10 +3,9 @@ const {messageRequest} = require('../controllers/controller.messagecontroller');
 const {messageIDRequest} = require('../controllers/controller.messageIDcontroller');
 const router = express.Router();
 
-// define the home page route
 router.get('/', messageRequest);
 router.get('/:myID', messageIDRequest);
 
-// define the about route
+
 
 module.exports = router
